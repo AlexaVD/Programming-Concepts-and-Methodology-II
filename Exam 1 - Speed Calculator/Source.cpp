dbacc1e3ddc6f7da50f96824e6598689fa42f4e6
Alexa <114698741+AlexaVD@users.noreply.github.com>
2022-12-07 13:59:17 -0800

    Add files via upload

---
+++ Exam 1 - Speed Calculator/Source.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main() {
+    int month;
+
+    month = 1;
+    switch (month)
+    {
+    case 1:
+        cout << "A";
+       
+    case 2:
+        cout << "B";
+    default:
+        cout << "C";
+    }
+
+
+    return 0;
+}
+
